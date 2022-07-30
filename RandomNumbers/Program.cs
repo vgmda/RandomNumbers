bd1de1109267f3165abcf2f9111c109797f7a283
vgmda <vasilegancin@gmail.com>
2022-07-30 23:27:31 +0100

    Random numbers

--- RandomNumbers/Program.cs
+++ RandomNumbers/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RandomNumbers;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    //SimpleMethod(random);
+        //    Console.WriteLine(random.NextDouble() * 10);
+        //}
+
+        List<PersonModel> people = new List<PersonModel>
+        {
+            new PersonModel{FirstName = "Sue"},
+            new PersonModel{FirstName = "Bob"},
+            new PersonModel{FirstName = "David"},
+            new PersonModel{FirstName = "Arnold"},
+            new PersonModel{FirstName = "Jack"},
+            new PersonModel{FirstName = "Zoe"},
+            new PersonModel{FirstName = "Sophie"}
+        };
+
+
+    }
+
+    private static void SimpleMethod(Random random)
+    {
+        Console.WriteLine(random.Next());
+    }
+
+}
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+}
